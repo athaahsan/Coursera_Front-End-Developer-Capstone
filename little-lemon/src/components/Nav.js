f48c49f713aa0d6a42603d8a97a4d54e13dded71
Atha Ahsan Xavier Haris <atha.ahsan.xavier.haris.com>
2024-04-16 21:54:40 +0700

    progress

---
+++ little-lemon/src/components/Nav.js
@@ -0,0 +1,14 @@
+const Nav = () => {
+    return (
+        <nav>    
+            <ul>
+                <li><a href="/home">Homepage</a></li>        
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/blog">Blog</a></li> 
+                <li><a href="/book" role="button">Book a table</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
